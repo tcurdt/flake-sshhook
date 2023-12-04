696a3cb0edc4bacc11225d61e23374adc7acef6e
Torsten Curdt <tcurdt@vafer.org>
2023-12-04 21:58:00 +0100

    current state

--- flake.nix
+++ flake.nix
@@ -5,54 +5,11 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
   };
 
-  # outputs = _: {
-  #   nixosModules.myconfig = { self, nixpkgs }: {
-  #     users.users.foo = {
-  #       isNormal = true;
-  #     };
-  #     security.sudo = {
-  #       enable = true;
-  #       extraRules = [{
-  #         commands = [
-  #           {
-  #             command = "FIXME path to script from within the flake";
-  #             options = [ "NOPASSWD" ];
-  #           }
-  #         ];
-  #         groups = [ "wheel" ]; # FIXME only the user "foo"
-  #       }];
-  #     };
-  #   };
-  # };
-
-  # outputs = { self, nixpkgs }: {
-  #   nixosModules = {
-  #     myconfig = {
-  #       users.users.foo = {
-  #         isNormal = true;
-  #       };
-  #       security.sudo = {
-  #         enable = true;
-  #         extraRules = [{
-  #           commands = [
-  #             {
-  #               command = "FIXME path to script from within the flake";
-  #               options = [ "NOPASSWD" ];
-  #             }
-  #           ];
-  #           groups = [ "wheel" ]; # FIXME only the user "foo"
-  #         }];
-  #       };
-  #     };
-  #   };
-  # };
-
   outputs = { self, nixpkgs }: {
-
-    nixosModules = {
+    nixosModules.default = {
 
       users.users.foo = {
-        isNormal = true;
+        isNormalUser = true;
       };
 
       security.sudo = {
@@ -69,7 +26,5 @@
       };
 
     };
-
   };
-
 }
